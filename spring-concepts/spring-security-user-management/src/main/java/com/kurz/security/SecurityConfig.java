5dc058dc9263210a44e0d2f192f0d9686ad70497
alexcastrodev <alexoliveira7x@gmail.com>
2026-07-29 23:24:47 +0100

    guide for reader

---
+++ spring-concepts/spring-security-user-management/src/main/java/com/kurz/security/SecurityConfig.java
@@ -0,0 +1,16 @@
+package com.kurz.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+/**
+ * TODO: Implement Spring Security configuration
+ */
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    // TODO: Implement security configuration methods
+
+}
