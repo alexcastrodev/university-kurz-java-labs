4eb79614c9fa2aece4ec8723d32366cb0680a833
alexcastrodev <alexoliveira7x@gmail.com>
2026-07-29 23:55:29 +0100

    solve array comparator

--- java-minute/array-equality/src/main/java/ArrayComparator.java
+++ java-minute/array-equality/src/main/java/ArrayComparator.java
@@ -13,7 +13,7 @@ public class ArrayComparator {
         // TODO-00: Use the appropriate method from java.util.Arrays to compare single-dimensional arrays.
         // Hint: Look for a method that compares array content, not references.
 
-        throw new UnsupportedOperationException("Not implemented yet.");
+        return Arrays.equals(first, second);
     }
 
     /**
@@ -27,6 +27,6 @@ public class ArrayComparator {
         // TODO-01: Use the appropriate method from java.util.Arrays for multi-dimensional arrays.
         // Hint: This method recurses into nested arrays.
 
-        throw new UnsupportedOperationException("Not implemented yet.");
+        return Arrays.deepEquals(first, second);
     }
 }
