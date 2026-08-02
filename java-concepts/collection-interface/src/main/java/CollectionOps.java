4d89bbaff3ef29ba05a84a70b25dda400cc7d50c
alexcastrodev <alexoliveira7x@gmail.com>
2026-08-02 18:53:15 +0100

    finish collection lab

--- java-concepts/collection-interface/src/main/java/CollectionOps.java
+++ java-concepts/collection-interface/src/main/java/CollectionOps.java
@@ -7,24 +7,33 @@ public class CollectionOps {
         // TODO-00: Return a NEW collection containing only elements found in both a and b.
         // Hint: copy 'a' into a new mutable collection first, then use the Collection
         // method that keeps only what's also present in 'b'. Do not mutate a or b.
-        throw new UnsupportedOperationException("Not implemented yet.");
+        Collection<String> intersection = new ArrayList<>(a);
+        intersection.retainAll(b);
+
+        return intersection;
     }
 
     public static Collection<String> difference(Collection<String> a, Collection<String> b) {
         // TODO-01: Return a NEW collection containing elements in 'a' that are NOT in 'b'.
         // Do not mutate a or b.
-        throw new UnsupportedOperationException("Not implemented yet.");
+        Collection<String> difference = new ArrayList<>(a);
+        difference.removeAll(b);
+
+        return difference;
     }
 
     public static Collection<String> removeBlank(Collection<String> source) {
         // TODO-02: Return a new collection like 'source' but with blank/empty strings removed.
         // Hint: copy first, then use removeIf with a Predicate. Do not mutate source.
-        throw new UnsupportedOperationException("Not implemented yet.");
+        Collection<String> notBlank = new ArrayList<>(source);
+        notBlank.removeIf(String::isBlank);
+
+        return notBlank;
     }
 
     public static String[] toStringArray(Collection<String> source) {
         // TODO-03: Convert source to a properly-typed String[] (not Object[]).
         // Hint: use the toArray overload that takes an IntFunction<T[]> generator.
-        throw new UnsupportedOperationException("Not implemented yet.");
+        return source.toArray(String[]::new);
     }
 }
